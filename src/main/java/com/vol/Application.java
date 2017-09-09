bb822705ce325134bbd6a5747a48a916bbdf6c5f
zhaorui <piaomiao775@gmail.com>
2017-09-09 08:07:03 +0800

    修改事务处理

--- src/main/java/com/vol/Application.java
+++ src/main/java/com/vol/Application.java
@@ -1,11 +1,18 @@
 package com.vol;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 
 /**
@@ -21,11 +28,22 @@ public class Application {
 
     private static Logger logger = LogManager.getLogger(Application.class);
 
+    @Bean(initMethod = "init", destroyMethod = "close")
+    @ConfigurationProperties(prefix = "spring.datatsource")
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
+
     /**
      * Main Start
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         logger.info("============= SpringBoot Start Success =============");
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 }
